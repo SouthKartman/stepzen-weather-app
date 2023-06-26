999a67ba9a501a738a7d3119b763c970a9c4fe74
Tema <93534577+SouthKartman@users.noreply.github.com>
2023-06-26 10:33:26 +0300

    citypecker ready

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -1,12 +1,57 @@
-import { Country, City } from "country-state-city"
-import Select from "react-select/dist/declarations/src/Select"
+import {Country, City} from "country-state-city";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+import Select from "react-select";
 
-function CityPicker() {
-  return (
-    <div className="">
-        
-    </div>
-  )
+type option = {
+    value:{
+        latitude:string;
+        longitude:string;
+        isoCode:string;
+      };
+      label: string;
+} | null;
+
+type cityOption = {
+    value:{
+        latitude: string;
+        longitude: string;
+        name: string;
+        countryCode: string;
+        stateCode: string;
+      };
+      label: string;
+} | null;
+
+const options = Country.getAllCountries().map ((Country) => ({
+    value:{
+      latitude: Country.latitude,
+      longitude:Country.latitude,
+      isoCode:Country.latitude
+    },
+    label: Country.name,
+  }));
+
+
+export default function CityPicker (){
+    const [selectedCountry,setSelectedCountry] = useState<option>(null);
+    const [selectedCity, setSelectedCity] = useState<cityOption>(null);
+    const router = useRouter();
+
+    const handleSelectedCountry = (option: option) =>
+    {
+        setSelectedCountry(option);
+        setSelectedCity(null);
+    }
+    return <div>
+            <div>
+                <label htmlFor="country"></label>
+            </div>
+                <Select 
+                    className="text-black"
+                    value={selectedCountry} 
+                    options={options}
+                    onChange={handleSelectedCountry}
+                />
+            </div>
 }
-
-export default CityPicker
